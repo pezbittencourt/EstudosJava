d9717b1225818072689b5f7f7e655cf79579ed57
Pedro Zeferino Bittencourt <pedro.zeferinob@gmail.com>
2025-07-16 10:00:38 -0300

    Add files via upload

---
+++ maratonaJava/introducao/Aula03Exercicios.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula03Exercicios {
+
+    public static void main(String[] args) {
+         String nome, endereco;
+         nome = "Pedro Zeferino Bittencourt";
+        endereco = "Rua Casa do Ator, 803";
+        float salario = 1900;
+        int paymentDay = 5;
+
+        System.out.println("Eu, " +nome+ ", morando no endereco "+endereco+", confirmo " +
+                "que recebi o salario de "+salario+", no dia "+paymentDay);
+
+
+
+
+    }
+}
