c1236f8772284324470ae8ede1bdc1f774e00b29
Pedro Zeferino Bittencourt <pedro.zeferinob@gmail.com>
2025-07-20 14:58:13 -0300

    Add files via upload

--- maratonaJava/introducao/Arrays/Aula07Arrays01.java
+++ maratonaJava/introducao/Arrays/Aula07Arrays01.java
@@ -2,9 +2,11 @@ package academy.devdojo.maratonaJava.introducao.Arrays;
 
 public class Aula07Arrays01 {
     public static void main(String[] args) {
-       int idades [] = new int[3];
-
+        int i=3;
+       int idades [] = new int[i];
+       
         System.out.println(idades[0]);
-
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
     }
 }
